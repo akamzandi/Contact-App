513cc3a067b913d4b972c32b0ee85ce82dd85cca
Akam Zandi <zandi.dev@gmail.com>
2022-08-21 22:16:49 +0430

    tags page layout edited

--- src/Pages/Tags/TagsPage.js
+++ src/Pages/Tags/TagsPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { ChromePicker } from "react-color";
 import Tag from "../../Components/Tag/Tag";
+import "../../Pages/Tags/tagsPage.css";
 
 const TagsPage = ({
   allTags,
@@ -42,12 +43,14 @@ const TagsPage = ({
     <div className="tagsPage">
       <h3>Tags Page</h3>
       <div className="tagsPage-container">
+        <p className="sub-headline">Tags List</p>
         <div className="tags-container">
-          <p className="sub-headline">Tags List:</p>
+          {/* <p className="sub-headline">Tags List:</p> */}
           {renderTags()}
         </div>
+        <p className="sub-headline">{editTagMode ? "Edit Tag" : "Add Tag"}</p>
         <div className="tags-controlPanel">
-          <p className="sub-headline">{editTagMode ? "Edit Tag" : "Add Tag"}</p>
+          {/* <p className="sub-headline">{editTagMode ? "Edit Tag" : "Add Tag"}</p> */}
           <form onSubmit={(e) => submitHandlerTag(e)}>
             <section className="tag-input-section">
               <label htmlFor="tag-name">Tag Name</label>
