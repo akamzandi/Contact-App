feae25f899227c0eadc0ce70e807809d0c694eca
Akam Zandi <zandi.dev@gmail.com>
2022-08-22 15:07:23 +0430

    tags page -> tags restyled

--- src/Components/Tag/Tag.js
+++ src/Components/Tag/Tag.js
@@ -1,17 +1,20 @@
+import "../../Components/Tag/tag.css";
+
 const Tag = ({ tag, deleteTagHandler, editTagHandler, id }) => {
   const renderTagStyle = (inputColor) => {
     return {
-      padding: "5px 10px",
-      margin: "5px",
-      color: "black",
-      borderLeft: `3px solid ${inputColor}`,
+      // padding: "5px 10px",
+      // margin: "5px",
+      // color: "black",
+      // borderLeft: `3px solid ${inputColor}`,
+      backgroundColor: inputColor,
     };
   };
   return (
     <div className="tag-container">
-      <p key={id} style={renderTagStyle(tag.color)}>
-        {tag.label}
-      </p>
+      <div className="tag-info-container" style={renderTagStyle(tag.color)}>
+        <p key={id}>{tag.label}</p>
+      </div>
       <div className="tag-buttons">
         <button className="edit-button" onClick={() => editTagHandler(id)}>
           Edit
