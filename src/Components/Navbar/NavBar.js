523ffa33b3f718e7f299c3725e10a45a0d7ac337
Akam Zandi <zandi.dev@gmail.com>
2022-08-24 15:09:10 +0430

    mobile menu and active styles corrected

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -6,33 +6,33 @@ const NavBar = ({ editContactMode }) => {
     if (editContactMode) {
       return (
         <>
-          <p className="contactList-link">Contact List</p>
           <NavLink
             className={({ isActive }) => (isActive ? "active-link" : "")}
             to="addEditContact"
           >
             {editContactMode ? "Edit Contact" : "Add Contact"}
           </NavLink>
-          <p className="contactList-link">tags</p>
+          <p className="disabled-nav-link">Contact List</p>
+          <p className="disabled-nav-link">Tags</p>
         </>
       );
     } else {
       return (
         <>
           <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : "")}
+            className={({ isActive }) => (isActive ? "active-link " : "")}
             to="addEditContact"
           >
             {editContactMode ? "Edit Contact" : "Add Contact"}
           </NavLink>
           <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : "")}
+            className={({ isActive }) => (isActive ? "active-link " : "")}
             to="/"
           >
             Contacts List
           </NavLink>
           <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : "")}
+            className={({ isActive }) => (isActive ? "active-link " : "")}
             to="tags"
           >
             Tags
